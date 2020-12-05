266951f9bd504502e9dcec1b6927bffc4336357b
Jason Serafica <seraficaj@gmail.com>
2020-12-05 10:51:21 -0800

    set up css theme

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,16 @@
 console.log("connected to app.js!");
+
+const myOverlay = document.getElementById("myOverlay");
+const mySidebar = document.getElementById("mySidebar");
+const navCloseMenu = document.getElementById("nav-close-menu");
+const navHamburger = document.getElementById("nav-hamburger");
+
+navHamburger.addEventListener('click', () => {
+   mySidebar.style.display = "block";
+   myOverlay.style.display = "block";
+});
+
+navCloseMenu.addEventListener('click', () => {
+   mySidebar.style.display = "none";
+   myOverlay.style.display = "none";
+})
